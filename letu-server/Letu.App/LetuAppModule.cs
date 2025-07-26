489f46041e3d051ec29ec7eaf6eab4103c16148a
letu <stephenevanstehi@gmail.com>
2025-07-26 14:02:36 +0800

    添加App入口项目。

---
+++ letu-server/Letu.App/LetuAppModule.cs
@@ -0,0 +1,15 @@
+﻿using Letu.Core.AutoInject;
+using Letu.Core.Context;
+
+namespace Letu.App;
+
+public class LetuAppModule : ModuleBase
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+
+    public override void Configure(ApplicationInitializationContext app)
+    {
+    }
+}
